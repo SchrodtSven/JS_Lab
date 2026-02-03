62a81ba9905e8f09998d823235fbab5b129db74f
Sven Schrodt <sven@schrodt.club>
2026-02-03 10:59:24 +0100

    Cleaning bot ...

--- lib/Mvc/Request.php
+++ lib/Mvc/Request.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 /**
  * HTTP request
  * 
- 
  * @author Sven Schrodt<sven@schrodt.club>
  * @link https://github.com/SchrodtSven/JSLab
  * @package JSLab
  * @version 0.1
- * @since 2021-01-26
+ * @since 2026-01-26
  */
 
 namespace SchrodtSven\JSLab\Mvc;
